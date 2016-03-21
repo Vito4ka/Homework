30b71987f91fe15dd5c63a3417fb27b3c5b73dbe
Игорь Теплов < >
2016-03-22 00:56:28 +0200

    it works!

--- js_03-04/js/script.js
+++ js_03-04/js/script.js
@@ -126,3 +126,10 @@ elementForm.appendChild(elementInput);
 
 
 document.body.appendChild(elementForm);
+var obj = {a: 1};
+
+var increment = function(obj ) { obj = {a: 2} }
+
+increment(obj);
+
+console.log(obj.a);
--- js_05-06/js/script.js
+++ js_05-06/js/script.js
@@ -1,31 +1,23 @@
 function startTimer() {
-   var my_timer = document.getElementById("my_timer");
-   var time = my_timer.innerHTML;
-   var arr = time.split(":");
-   var h = arr[0];
-   var m = arr[1];
-   var s = arr[2];
-   var ms = arr[3];
-   if (ms == 60) {
-     s++;
-     if (s == 60) {
-       m++;
-       if (m == 60) {
-         m++;
-         if (h == 24) {
-         h=0;
-       }
-       h--;
-       m = 60;
-     }
-     m--;
-     s = 59;
-   }
-   if (ms < 100) m = "0" + m;
-   if (ms < 10) m = "00" + m;
-   if (s < 10) s = "0" + s;
-   if (m < 10) m = "0" + m;
-   if (h < 10) h = "0" + h;
-   document.getElementById("my_timer").innerHTML = h+":"+m+":"+s+":"+ms;
-   setTimeout(startTimer, 1);
- }
+    var d = new Date();
+    var t = d.getTime();
+    var end=0;
+    var timer = setInterval( function () {
+  	 if (end==1) return;
+     var newDate = new Date();
+     var newT = newDate.getTime()-t;
+     var d = new Date;
+     d.setTime(newT);
+      var h=d.getHours()-2;
+      var m=d.getMinutes();
+      var s=d.getSeconds();
+      var ms=d.getMilliseconds();
+     document.getElementById("my_timer").innerHTML =  addZero(h,2)+":"+addZero(m,2)+":"+addZero(s,2)+"."+addZero(ms,3);
+ }, 1, t, end);
+}
+function addZero(x,n) {
+    while (x.toString().length < n) {
+        x = "0" + x;
+    }
+    return x;
+}
