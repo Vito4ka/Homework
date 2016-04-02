30280dade6d7270ed39493ca337dc5815fdb4928
Игорь Теплов < >
2016-04-02 09:13:07 +0300

    first part 11-12

--- js_11-12/js/slider.js
+++ js_11-12/js/slider.js
@@ -1,25 +1,9 @@
-$(document).ready(function() {
-    var leftUIEl = $('.slider-arrow-left');
-    var rightUIEl = $('.slider-arrow-right');
-    var elementsList = $('.slider-list');
+  $(function() {
 
-    var pixelsOffset = 125;
-    var currentLeftValue = 0;
-    var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
-    var maximumOffset = 0;
+$('.carousel').carousel({
+  delay: 500,
+})
 
-    leftUIEl.click(function() {
-          if (currentLeftValue != maximumOffset) {
-            currentLeftValue += 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
 
-    rightUIEl.click(function() {
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
-});
+
+  });
